08f85c4110ba4a8da2e738af072effb24bbd5b32
yebyte <yeyiwei910@live.cn>
2020-02-09 10:14:43 +0800

    Patch: centeredmaster

--- config.def.h
+++ config.def.h
@@ -49,6 +49,8 @@ static const Layout layouts[] = {
 	{ "[M]",      monocle },
 	{ "TTT",      bstack },
 	{ "===",      bstackhoriz },
+	{ "|M|",      centeredmaster },
+	{ ">M>",      centeredfloatingmaster },
     { "[D]",      deck },
 };
 
