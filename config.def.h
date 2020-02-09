4c409ed02ebd343ac6177db417b227fb0d26b6ee
yebyte <yeyiwei910@live.cn>
2020-02-09 09:49:47 +0800

    Patch: gaplessgrid

--- config.def.h
+++ config.def.h
@@ -38,10 +38,12 @@ static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
 #include "fibonacci.c"
+#include "gaplessgrid.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[@]",      spiral },
 	{ "[\\]",     dwindle },
+	{ "###",	  gaplessgrid },
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
